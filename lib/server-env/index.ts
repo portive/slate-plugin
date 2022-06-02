907955a4d9f47f1f1c98f4cd325beb6a0f125641
Sunny Hirai <thesunny@gmail.com>
2022-06-01 22:26:19 -0700

    Remove api key requirement

--- lib/server-env/index.ts
+++ lib/server-env/index.ts
@@ -1,11 +1,7 @@
-const { PORTIVE_API_KEY, PORTIVE_AUTH_TOKEN } = process.env
-
-if (PORTIVE_API_KEY === undefined) {
-  throw new Error(`Expected process.env.PORTIVE_API_KEY to be defined`)
-}
+const { PORTIVE_AUTH_TOKEN } = process.env
 
 if (PORTIVE_AUTH_TOKEN === undefined) {
   throw new Error(`Expected process.env.PORTIVE_AUTH_TOKEN to be defined`)
 }
 
-export const env = { PORTIVE_API_KEY, PORTIVE_AUTH_TOKEN }
+export const env = { PORTIVE_AUTH_TOKEN }
