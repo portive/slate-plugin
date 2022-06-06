2bd75606bca294aa28a58c91645702f17881cfaf
Sunny Hirai <thesunny@gmail.com>
2022-06-05 17:11:39 -0700

    Finish refactor status-bar

--- lib/portive/render-image/progress-bar.tsx
+++ lib/portive/render-image/progress-bar.tsx
@@ -1,58 +1,5 @@
 import { Origin } from "../types"
 
-export function _ProgressBar({
-  className,
-  sentBytes,
-  totalBytes,
-  width,
-  height,
-  style,
-}: {
-  className?: string
-  sentBytes: number
-  totalBytes: number
-  width: number
-  height: number
-  style?: React.CSSProperties
-}) {
-  /**
-   * This formula looks a little funny because we want the `0` value of the
-   * progress bar to have a width that is still the height of the progress bar.
-   *
-   * This is for a few reasons:
-   *
-   * 1. We want the zero point to start with the progress bar being a circle
-   * 2. If we want rounded edges, if the width is shorter than the height,
-   *    we get an oval instead of a circle
-   * 3. The halfway point looks visually wrong because of the circle progress
-   *    bar when it is technically at the halfway point.
-   */
-  const progressWidth = (sentBytes / totalBytes) * (width - height) + height
-
-  return (
-    <div
-      className={className}
-      style={{
-        ...style,
-        width,
-        height,
-        background: "white",
-        borderRadius: height / 2,
-      }}
-    >
-      <div
-        style={{
-          background: "DodgerBlue",
-          width: progressWidth,
-          transition: "width 0.1s",
-          height,
-          borderRadius: height / 2,
-        }}
-      ></div>
-    </div>
-  )
-}
-
 export function ProgressBar({
   origin,
   className,
@@ -69,18 +16,42 @@ export function ProgressBar({
   if (origin.status !== "uploading") {
     return null
   }
+  /**
+   * This formula looks a little funny because we want the `0` value of the
+   * progress bar to have a width that is still the height of the progress bar.
+   *
+   * This is for a few reasons:
+   *
+   * 1. We want the zero point to start with the progress bar being a circle
+   * 2. If we want rounded edges, if the width is shorter than the height,
+   *    we get an oval instead of a circle
+   * 3. The halfway point looks visually wrong because of the circle progress
+   *    bar when it is technically at the halfway point.
+   */
+  const progressWidth =
+    (origin.sentBytes / origin.totalBytes) * (width - height) + height
   return (
-    <_ProgressBar
+    <div
       className={className}
-      sentBytes={origin.sentBytes}
-      totalBytes={origin.totalBytes}
-      width={width}
-      height={height}
       style={{
+        width,
+        height,
+        background: "white",
+        borderRadius: height / 2,
         boxShadow: "0 0 1px 0px rgba(0,0,0,1)",
         ...style,
       }}
-    />
+    >
+      <div
+        style={{
+          background: "DodgerBlue",
+          width: progressWidth,
+          transition: "width 0.1s",
+          height,
+          borderRadius: height / 2,
+        }}
+      ></div>
+    </div>
   )
 }
 
@@ -114,6 +85,7 @@ export function ErrorBar({
         textAlign: "center",
         textTransform: "uppercase",
         borderRadius: height / 2,
+        boxShadow: "0 0 1px 0px rgba(0,0,0,1)",
         ...style,
       }}
     >
@@ -138,10 +110,9 @@ export function StatusBar({
   switch (origin.status) {
     case "uploading":
       return (
-        <_ProgressBar
+        <ProgressBar
+          origin={origin}
           className={className}
-          sentBytes={origin.sentBytes}
-          totalBytes={origin.totalBytes}
           width={width}
           height={height}
           style={{
@@ -152,26 +123,16 @@ export function StatusBar({
       )
     case "error":
       return (
-        <div
+        <ErrorBar
+          origin={origin}
           className={className}
+          width={width}
+          height={height}
           style={{
-            width,
-            height,
-            fontSize: "75%",
-            fontWeight: "bold",
-            lineHeight: `${height}px`,
-            color: "rgba(255, 255, 255, 0.9)",
-            background: "FireBrick",
-            textAlign: "center",
-            textTransform: "uppercase",
-            borderRadius: height / 2,
             ...style,
           }}
-        >
-          Upload Failed
-        </div>
+        />
       )
-      break
     case "complete":
       return null
     default:
